5d8385eb2e5f746f9a4e0a03c7f0123650f08b40
vaibhav29 <vaibhav@DESKTOP-9465059>
2020-09-22 14:10:32 +0530

    test

---
+++ scripts.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE [EmpMasterDetails] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NULL,
+    [UserId] nvarchar(max) NULL,
+    [MobiileNumber] INT NULL,
+    [Region] nvarchar(max) NULL
+    CONSTRAINT [PK_EmpMasterDetails] PRIMARY KEY ([Id])
+);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20200919081937_addedtableemployemasterdetils', N'3.1.8');
+
+GO
+
+CREATE TABLE [EmpMasterDetails] (
+    [Id] int NOT NULL IDENTITY,
+    [EmailId] nvarchar(max) NULL,
+    [City] nvarchar(max) NULL,
+    [Designation] nvarchar(max) NULL,
+    [Address] nvarchar(max) NULL,
+    [EmployeeMasterId] int NOT NULL,
+    CONSTRAINT [PK_EmpMasterDetails] PRIMARY KEY ([Id])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20200919085141_addedtableemployemasterdetilsgh', N'3.1.8');
+
+GO
+
